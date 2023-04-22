ac45ce44107db09bf6dc8c92883670a9582f4eba
VanPhuong03 <boss0352827813@gmail.com>
2023-04-22 10:06:34 +0700

    question

---
+++ main.js
@@ -0,0 +1,216 @@
+
+function checkAnswer1() {
+    var inputElement = document.getElementById("question1");
+    var inputValue = inputElement.value;
+    if(inputValue=="")
+    {
+        var para = document.getElementById("correct1");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect1");
+         no.style.display = "none";
+    }
+    else if(inputValue==1911)
+    {
+        var para = document.getElementById("correct1");
+         para.style.display = "block";
+         var no = document.getElementById("incorrect1");
+         no.style.display = "none";
+    }
+    else{
+        var para = document.getElementById("correct1");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect1");
+         no.style.display = "block";
+    }
+  }
+  function resetAnswer1(){
+    var para = document.getElementById("correct1");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect1");
+         no.style.display = "none";
+  }
+  function nextQuestion1(){
+    var close = document.querySelector('.answer')
+       close.style.display="none"
+    var close = document.querySelector('.question1')
+       close.style.display="none"
+       var close = document.querySelector('.question2')
+       close.style.display="block"
+  }
+
+
+
+  function checkAnswer2() {
+    var inputElement = document.getElementById("question2");
+    var inputValue = inputElement.value;
+    if(inputValue=="")
+    {
+        var para = document.getElementById("correct2");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect2");
+         no.style.display = "none";
+    }
+    else if(inputValue==108)
+    {
+        var para = document.getElementById("correct2");
+         para.style.display = "block";
+         var no = document.getElementById("incorrect2");
+         no.style.display = "none";
+    }
+    else{
+        var para = document.getElementById("correct2");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect2");
+         no.style.display = "block";
+    }
+  }
+  function resetAnswer2(){
+    var para = document.getElementById("correct2");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect2");
+         no.style.display = "none";
+  }
+  function nextQuestion2(){
+    var close = document.querySelector('.answer')
+       close.style.display="none"
+    var close = document.querySelector('.question2')
+       close.style.display="none"
+       var close = document.querySelector('.question3')
+       close.style.display="block"
+  }
+
+
+
+  function checkAnswer3() {
+    var inputElement = document.getElementById("question3");
+    var inputValue = inputElement.value;
+    if(inputValue=="")
+    {
+        var para = document.getElementById("correct3");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect3");
+         no.style.display = "none";
+    }
+    else if(inputValue==1885)
+    {
+        var para = document.getElementById("correct3");
+         para.style.display = "block";
+         var no = document.getElementById("incorrect3");
+         no.style.display = "none";
+    }
+    else{
+        var para = document.getElementById("correct3");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect3");
+         no.style.display = "block";
+    }
+  }
+  function resetAnswer3(){
+    var para = document.getElementById("correct3");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect3");
+         no.style.display = "none";
+  }
+  function nextQuestion3(){
+    var close = document.querySelector('.answer')
+       close.style.display="none"
+    var close = document.querySelector('.question1')
+       close.style.display="none"
+       var close = document.querySelector('.question2')
+       close.style.display="none"
+       var close = document.querySelector('.question3')
+       close.style.display="none"
+       var close = document.querySelector('.question4')
+       close.style.display="block"
+  }
+
+
+
+  function checkAnswer4() {
+    var inputElement = document.getElementById("question4");
+    var inputValue = inputElement.value;
+    if(inputValue=="")
+    {
+        var para = document.getElementById("correct4");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect4");
+         no.style.display = "none";
+    }
+    else if(inputValue==1949)
+    {
+        var para = document.getElementById("correct4");
+         para.style.display = "block";
+         var no = document.getElementById("incorrect4");
+         no.style.display = "none";
+    }
+    else{
+        var para = document.getElementById("correct4");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect4");
+         no.style.display = "block";
+    }
+  }
+  function resetAnswer4(){
+    var para = document.getElementById("correct4");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect4");
+         no.style.display = "none";
+  }
+  function nextQuestion4(){
+    var close = document.querySelector('.answer')
+       close.style.display="none"
+    var close = document.querySelector('.question1')
+       close.style.display="none"
+       var close = document.querySelector('.question2')
+       close.style.display="none"
+       var close = document.querySelector('.question3')
+       close.style.display="none"
+       var close = document.querySelector('.question4')
+       close.style.display="none"
+       var close = document.querySelector('.question5')
+       close.style.display="block"
+  }
+  function checkAnswer5() {
+    var inputElement = document.getElementById("question5");
+    var inputValue = inputElement.value;
+    if(inputValue=="")
+    {
+        var para = document.getElementById("correct5");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect5");
+         no.style.display = "none";
+    }
+    else if(inputValue==1925)
+    {
+        var para = document.getElementById("correct5");
+         para.style.display = "block";
+         var no = document.getElementById("incorrect5");
+         no.style.display = "none";
+    }
+    else{
+        var para = document.getElementById("correct5");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect5");
+         no.style.display = "block";
+    }
+  }
+  function resetAnswer5(){
+    var para = document.getElementById("correct5");
+         para.style.display = "none";
+         var no = document.getElementById("incorrect5");
+         no.style.display = "none";
+  }
+  function nextQuestion5(){
+    var close = document.querySelector('.answer')
+       close.style.display="none"
+    var close = document.querySelector('.question1')
+       close.style.display="none"
+       var close = document.querySelector('.question2')
+       close.style.display="none"
+       var close = document.querySelector('.question3')
+       close.style.display="none"
+       var close = document.querySelector('.question4')
+       close.style.display="none"
+       var close = document.querySelector('.question5')
+       close.style.display="none"
+  }
